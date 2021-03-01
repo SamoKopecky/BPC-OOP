937e58d1ca157f922c8f0e287eb0dca2a937db85
Samo Kopecky <sano.kopecky@gmail.com>
2021-03-01 15:03:23 +0100

    CV04: Initial upload

---
+++ CV04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CV04
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var stringTest = "Toto je retezec predstavovany nekolika radky,\n"
+                             + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                             + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                             + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                             + "posledni veta!";
+            StringStatistics stringStatistics = new StringStatistics(stringTest);
+            Console.WriteLine($"Pocet slov: {stringStatistics.WordCount()}");
+            Console.WriteLine($"Pocet radku: {stringStatistics.LineCount()}");
+            Console.WriteLine($"Pocet vet: {stringStatistics.SentenceCount()}");
+            Console.WriteLine($"Nejdelsi slova: {string.Join(", ", stringStatistics.LongestWords())}");
+            Console.WriteLine($"Nejkratsi slova: {string.Join(", ", stringStatistics.ShortestWords())}");
+            Console.WriteLine($"Najcetnejsi slova: {string.Join(", ", stringStatistics.MostCommonWords())}");
+            Console.WriteLine($"Slove dle abecedy: {string.Join(", ", stringStatistics.SortByAlphabet())}");
+        }
+    }
+}
---
+++ CV04/StringStatistics.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CV04
+{
+    class StringStatistics
+    {
+        public string Data { get; set; }
+        private readonly string[] _words;
+
+        public StringStatistics(string data)
+        {
+            Data = data;
+            _words = Data.Split(' ', '\n').Select(word =>
+            {
+                return new string(word.Where(character => !char.IsPunctuation(character)).ToArray());
+            }).ToArray();
+        }
+
+        public int WordCount() => _words.Length;
+
+
+        public int LineCount() => Data.Split('\n').Length;
+
+        public int SentenceCount()
+        {
+            var sentences = Data.Split('!', '?', '.');
+            sentences = sentences.Where(sentence => sentence != "").ToArray();
+            sentences = sentences.Where(sentence =>
+            {
+                if ((sentence[0] == ' ' && char.IsLower(sentence[1])) ||
+                    char.IsLower(sentence[0]))
+                {
+                    return false;
+                }
+
+                return true;
+            }).ToArray();
+            return sentences.Length;
+        }
+
+        public string[] LongestWords()
+        {
+            var orderedWords = _words.OrderByDescending(word => word.Length).ToArray();
+            var maxLength = orderedWords[0].Length;
+            return orderedWords.TakeWhile(word => word.Length == maxLength).ToArray();
+        }
+
+        public string[] ShortestWords()
+        {
+            var orderedWords = _words.OrderBy(word => word.Length).ToArray();
+            var minLength = orderedWords[0].Length;
+            return orderedWords.TakeWhile(word => word.Length == minLength).ToArray();
+        }
+
+        public string[] SortByAlphabet() => _words.OrderBy(word => word).ToArray();
+
+        public string[] MostCommonWords()
+        {
+            var occurrences = new Dictionary<string, int>();
+            foreach (var word in _words)
+            {
+                if (!occurrences.ContainsKey(word))
+                {
+                    occurrences.Add(word, 1);
+                }
+                else
+                {
+                    occurrences[word]++;
+                }
+            }
+
+            var max = occurrences.Values.Max();
+
+            return (from pair in occurrences where pair.Value == max select pair.Key).ToArray();
+        }
+    }
+}
